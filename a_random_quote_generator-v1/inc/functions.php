c2e97bcd3a29be303e3dc38df1d33587e6606e91
Emma Gomez Xolo <emmagomezxolo@gmail.com>
2020-11-09 14:54:01 -0500

    added new item to the arrays and added if statement to include it to the quotes. added random color changing background and button. Added meta tags to auto refresh the page every 10 seconds

--- a_random_quote_generator-v1/inc/functions.php
+++ a_random_quote_generator-v1/inc/functions.php
@@ -9,55 +9,61 @@ $quotes[] = [
 'quote' => 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
 'source' => 'Nelson Mandela',
 'citation' => 'blog.hubspot.com',
-'year' => ''
+'year' => '',
+'hashtag' => ''
 ];
 
 $quotes[] = [
 'quote' => 'Get busy living or get busy dying.',
 'source' => 'Stephen King',
 'citation' => 'keepinspiring.me',
-'year' => ''
+'year' => '',
+'hashtag' => ''
 ];
 
 $quotes[] = [
 'quote' => 'Every moment is a fresh beginning',
 'source' => 'T.S Eliot',
 'citation' => 'codeofliving.com',
-'year' => 1949
+'year' => 1949,
+'hashtag' => ''
 ];
 
 $quotes[] = [
 'quote' => 'I could agree with you but then we’d both be wrong.',
 'source' => 'Harvey Specter',
 'citation' => 'Suits',
-'year' => 2015
+'year' => 2015,
+'hashtag' => ''
 ];
 
 $quotes[] = [
 'quote' => 'Dream as if you’ll live forever, live as if you’ll die today.',
 'source' => 'James Dean',
 'citation' => 'codeofliving.com',
-'year' => ''
+'year' => '',
+'hashtag' => ''
 ];
 
 $quotes[] = [
-    'quote' => 'Nobody owes you a job.',
-    'source' => 'Steven H Haas',
-    'citation' => '',
-    'year' => '2020'
+'quote' => 'Nobody owes you a job.',
+'source' => 'Steven H Haas',
+'citation' => '',
+'year' => '2020',
+'hashtag' => '#myhusband'
 ];
 
 
 // Create the getRandomQuote function and name it getRandomQuote
-function getRandomQuote($arr){
-    $randomNumber = rand(0,sizeof($arr)-1);
-    return $arr[$randomNumber];
+function getRandomQuote($quotesArray){
+    $randomNumber = rand(0,sizeof($quotesArray)-1);
+    return $quotesArray[$randomNumber];
 }
 
 // Create the printQuote function and name it printQuote
 
-function printQuote ($arr) {
-    $randomQuote = getRandomQuote($arr);
+function printQuote ($quotesArray) {
+    $randomQuote = getRandomQuote($quotesArray);
 
     $combinedQuote = "<p class=\"quote\">" . $randomQuote['quote'] . "</p>" .
                          "<p class=\"source\">" .  $randomQuote['source'];
@@ -70,7 +76,18 @@ function printQuote ($arr) {
     if (!empty($randomQuote['year'])) {
         $combinedQuote = $combinedQuote . "<span class=\"year\">" . $randomQuote['year'] . "</span>";
     }
+    if (!empty($randomQuote['hashtag'])) {
+        $combinedQuote = $combinedQuote . "<span class=\"hashtag\">" . $randomQuote['hashtag'] . "</span>";
+    }
 
     return $combinedQuote . "</p>" ;
 
 }
+
+//function randomColor() {
+//$rand = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+//$color = '#' . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(
+//        0, 15)] . $rand[rand(0, 15)];
+//        return $color;
+//}
+
--- a_random_quote_generator-v1/index.php
+++ a_random_quote_generator-v1/index.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="10" >
+    <meta charset="UTF-8">
   <title>Random Quotes</title>
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/styles.css">
-
+  <style>/*changes background color on click*/
+    body {background-color: <?php
+        function randomColor() {
+            $rand = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+            $color = '#' . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(
+                      0, 15)] . $rand[rand(0, 15)];
+            return $color;
+};
+    echo randomColor();?>}
+  </style>
+    <style>/*changes button color on click*/
+        #loadQuote {background-color: <?php
+        function buttonColor() {
+            $rand = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+            $color = '#' . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(
+                      0, 15)] . $rand[rand(0, 15)];
+            return $color;
+};
+    echo buttonColor();?>}
+    </style>
 </head>
 <body>
   <div class="container">
@@ -15,8 +35,6 @@
         include 'inc/functions.php';
         echo printQuote($quotes);
       ?>
-      <!-- <p class="quote"></p>
-      <p class="source"><span class="citation"></span><span class="year"></span></p> -->
     </div>
     <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
   </div>
